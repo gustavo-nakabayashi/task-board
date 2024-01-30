f6c9e7e39580dd3e612028343a41487ecb7d20c4
Gustavo Nakabayashi <gustavo@disco-tec.com>
2024-01-30 18:54:03 -0300

    oth: move infra files

--- frontend/src/app/ui/TaskDialog.tsx
+++ frontend/src/app/ui/TaskDialog.tsx
@@ -25,8 +25,78 @@ type DialogProps = {
   setOpen: () => void;
 };
 
-const Dialog = () => {
-  const router = useRouter();
+const TaskDialog = ({ task }: { task: TaskType }) => {
+  const DescriptionInput = () => {
+    return (
+      <div className="flex flex-col">
+        <label htmlFor="Description" className="text-xs mb-1 text-[#97A3B6]">
+          Description
+        </label>
+
+        <input
+          className="border border-black px-4 py-1 rounded-md"
+          type="text"
+          name="Description"
+          defaultValue={task.Description}
+          placeholder="Enter a short description"
+        />
+      </div>
+    );
+  };
+
+  const NameInput = () => {
+    return (
+      <div className="flex flex-col">
+        <label className="text-xs mb-1 text-[#97A3B6]" htmlFor="Name">
+          Task name
+        </label>
+        <input
+          className="border border-black px-4 py-1 rounded-md"
+          type="text"
+          name="Name"
+          defaultValue={task.Name}
+        />
+      </div>
+    );
+  };
+
+  const IconInput = () => {
+    const iconsDivs = icons.map((icon) => {
+      return (
+        <label
+          class="flex h-10 w-10 text-xl rounded-lg justify-center items-center bg-[#E3E8EF] has-[:checked]:bg-[#F5D565] cursor-pointer"
+        >
+          {String.fromCodePoint(icon)}
+          <input name="icon" type="radio" class="hidden" />
+        </label>
+      );
+    });
+
+    return (
+      <div className="flex flex-col">
+        <label htmlFor="Icon" className="text-xs mb-1 text-[#97A3B6]">
+          Icon
+        </label>
+        <div className="flex gap-2">{iconsDivs}</div>
+      </div>
+    );
+  };
+
+  const StatusInput = () => {
+    return (
+      <div className="flex flex-col">
+        <label className="text-xs mb-1 text-[#97A3B6]" htmlFor="Status">
+          Status
+        </label>
+        <input type="text" name="Status" defaultValue={task.Status} />
+      </div>
+    );
+  };
+
+  if (task === undefined || task === null) {
+    console.log("task is undefined");
+    return null;
+  }
 
   return (
     <dialog
